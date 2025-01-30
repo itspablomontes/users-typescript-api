4538b8c2fef9bc347d7926501fcad185b055ecf9
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-01-30 15:51:01 -0300

    chore: express initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || "8000";
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+	res.send("Hello World");
+});
